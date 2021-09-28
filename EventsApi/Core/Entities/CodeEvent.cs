4f61443ff57a238529d71a434b74d57a57627a38
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2021-09-28 11:48:02 +0200

    Entities

---
+++ EventsApi/Core/Entities/CodeEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventsApi.Core.Entities
+{
+    public class CodeEvent
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime EventDate { get; set; }
+        public int Length { get; set; }
+
+        public int LocationId { get; set; }
+        public Location Location { get; set; }
+
+
+    }
+}
---
+++ EventsApi/Core/Entities/Lecture.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventsApi.Core.Entities
+{
+    public class Lecture
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Level { get; set; }
+
+        public int CodeEvevntId { get; set; }
+        public int SpeakerId { get; set; }
+        public CodeEvent CodeEvent { get; set; }
+        public Speaker Speaker { get; set; }
+    }
+}
