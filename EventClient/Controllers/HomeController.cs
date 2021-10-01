25c660f6eb0b2ca02981755b249fdd19b24574e2
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2021-10-01 14:00:59 +0200

    Simple Get

--- EventClient/Controllers/HomeController.cs
+++ EventClient/Controllers/HomeController.cs
@@ -1,37 +1,46 @@
 ﻿using EventClient.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace EventClient.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private HttpClient httpClient;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
+            httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://localhost:5001");
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> IndexAsync()
         {
+            var res = await SimpleGet();
+
             return View();
         }
 
-        public IActionResult Privacy()
+        private async Task<IEnumerable<CodeEventDto>> SimpleGet()
         {
-            return View();
-        }
+            
+            var response = await httpClient.GetAsync("api/events?includelectures=true");
+            response.EnsureSuccessStatusCode();
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var content = await response.Content.ReadAsStringAsync();
+            var codeEvents = JsonSerializer.Deserialize<IEnumerable<CodeEventDto>>(content, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+
+
+            return codeEvents;
+            
         }
     }
 }
